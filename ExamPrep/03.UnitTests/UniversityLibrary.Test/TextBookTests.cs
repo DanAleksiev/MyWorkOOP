c0256a100b669dbb303d67a6c90c3393a44ed172
DanAleksiev <toksikar46@gmail.com>
2023-03-25 18:20:01 +0000

    update

--- ExamPrep/03.UnitTests/UniversityLibrary.Test/TextBookTests.cs
+++ ExamPrep/03.UnitTests/UniversityLibrary.Test/TextBookTests.cs
@@ -1,18 +1,58 @@
 namespace UniversityLibrary.Test
 {
     using NUnit.Framework;
+    using System.Text;
+
     [TestFixture]
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
+        private string title = "The Adventures of Po";
+        private string author = "Bramborachek";
+        private string category = "Scy-Fi";
 
         [Test]
-        public void Test1()
-        {
-            Assert.Pass();
+        public void TextBookConstructor()
+            {
+            TextBook tb = new TextBook(title,author,category);
+
+            Assert.That(tb.Title, Is.EqualTo(title));
+            Assert.That(tb.Author, Is.EqualTo(author));
+            Assert.That(tb.Category, Is.EqualTo(category));
+            }
+
+        [Test]
+        public void TestToString()
+            {
+            TextBook tb = new TextBook(title, author, category);
+            tb.InventoryNumber = 1;
+
+            int inventoryNumber = 1;
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Book: {title} - {inventoryNumber}");
+            sb.AppendLine($"Category: {category}");
+            sb.AppendLine($"Author: {author}");
+
+            Assert.That(tb.ToString(), Is.EqualTo(sb.ToString().TrimEnd()));
+            }
+
+        [Test]
+        public void TestInvetoryNumbers()
+            {
+            TextBook tb = new TextBook(title, author, category);
+            int inventoryNumber = 1;
+            tb.InventoryNumber = inventoryNumber;
+
+            Assert.That(tb.InventoryNumber, Is.EqualTo(inventoryNumber));
+            }
+
+        [Test]
+        public void TestHolder()
+            {
+            string holder = "Frank";
+            TextBook tb = new TextBook(title, author, category);
+            tb.Holder = holder;
+
+            Assert.That(tb.Holder, Is.EqualTo(holder));
+            }
         }
-    }
 }
--- ExamPrep/03.UnitTests/UniversityLibrary/StartUp.cs
+++ ExamPrep/03.UnitTests/UniversityLibrary/StartUp.cs
@@ -1,10 +1,21 @@
-﻿namespace UniversityLibrary
-{
-    public class StartUp
+﻿using System.Collections.Generic;
+using UniversityLibrary;
+
+namespace UniversityLibrary
     {
-        static void Main(string[] args)
+    public class StartUp
         {
-            
+        static void Main(string[] args)
+            {
+            //UniversityLibraryDataBase db = new UniversityLibraryDataBase();
+            //List<TextBook> books = db.GetCatalogue();
+
+
+            //List<TextBook> books = new List<TextBook>();
+            //books.Add(new TextBook("DieHard", "Azis", "Action"));
+            //books.Add(new TextBook("IAmNumber4", "Paticus Lore", "Scy-Fi"));
+            //books.Add(new TextBook("Harry Potter", "J.K. Rawling", "Fiction"));
+            //db.SaveProducts(books);
+            }
         }
     }
-}
